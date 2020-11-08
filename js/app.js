d5b4bf873e8ea13fd4538fa9b6699ebe72cb0366
lsliwaradioluz <39009379+lsliwaradioluz@users.noreply.github.com>
2020-11-08 01:21:41 +0100

    write projectInput class, bind elements to hooks in dom, create validation logic

--- js/app.js
+++ js/app.js
@@ -5,6 +5,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+function validateInputs(validatableObject) {
+    let isValid = true;
+    const value = validatableObject.value;
+    if (validatableObject.required) {
+        isValid = isValid && value !== null;
+    }
+    if (validatableObject.minLength != null &&
+        typeof validatableObject.value === "string") {
+        isValid = isValid && value.toString().length >= validatableObject.minLength;
+    }
+    if (validatableObject.maxLength != null &&
+        typeof validatableObject.value === "string") {
+        isValid = isValid && value.toString().length <= validatableObject.maxLength;
+    }
+    if (validatableObject.min != null && typeof value === "number") {
+        isValid = isValid && value >= validatableObject.min;
+    }
+    if (validatableObject.max != null && typeof value === "number") {
+        isValid = isValid && value <= validatableObject.max;
+    }
+    return isValid;
+}
 // BindThis decorator
 function BindThis(_, _2, descriptor) {
     const originalMethod = descriptor.value;
@@ -30,9 +52,50 @@ class ProjectInput {
         this.configureForm();
         this.attach();
     }
+    gatherUserInput() {
+        const title = this.titleInputElement.value;
+        const description = this.descriptionInputElement.value;
+        const people = this.peopleInputElement.value;
+        const titleValidatable = {
+            value: title,
+            required: true,
+            minLength: 3,
+        };
+        const descriptionValidatable = {
+            value: description,
+            required: true,
+            minLength: 10,
+        };
+        const peopleValidatable = {
+            value: +people,
+            required: true,
+            min: 1,
+            max: 5,
+        };
+        if (!validateInputs(titleValidatable) ||
+            !validateInputs(descriptionValidatable) ||
+            !validateInputs(peopleValidatable)) {
+            alert("Invalid inputs!");
+            return;
+        }
+        else {
+            return [title, description, +people];
+        }
+    }
+    clearInputs() {
+        const inputNames = ["title", "description", "people"];
+        for (const name of inputNames) {
+            this[`${name}InputElement`].value = "";
+        }
+    }
     handleFormSubmit(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+            this.clearInputs();
+        }
     }
     configureForm() {
         this.formElement.addEventListener("submit", this.handleFormSubmit);
--- ts/app.ts
+++ ts/app.ts
@@ -1,3 +1,40 @@
+// Validation logic
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validateInputs(validatableObject: Validatable): boolean {
+  let isValid = true;
+  const value = validatableObject.value;
+  if (validatableObject.required) {
+    isValid = isValid && value !== null;
+  }
+  if (
+    validatableObject.minLength != null &&
+    typeof validatableObject.value === "string"
+  ) {
+    isValid = isValid && value.toString().length >= validatableObject.minLength;
+  }
+  if (
+    validatableObject.maxLength != null &&
+    typeof validatableObject.value === "string"
+  ) {
+    isValid = isValid && value.toString().length <= validatableObject.maxLength;
+  }
+  if (validatableObject.min != null && typeof value === "number") {
+    isValid = isValid && value >= validatableObject.min;
+  }
+  if (validatableObject.max != null && typeof value === "number") {
+    isValid = isValid && value <= validatableObject.max;
+  }
+  return isValid;
+}
+
 // BindThis decorator
 function BindThis(_: any, _2: string, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
@@ -46,10 +83,56 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    const title = this.titleInputElement.value;
+    const description = this.descriptionInputElement.value;
+    const people = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: title,
+      required: true,
+      minLength: 3,
+    };
+    const descriptionValidatable: Validatable = {
+      value: description,
+      required: true,
+      minLength: 10,
+    };
+    const peopleValidatable: Validatable = {
+      value: +people,
+      required: true,
+      min: 1,
+      max: 5,
+    };
+
+    if (
+      !validateInputs(titleValidatable) ||
+      !validateInputs(descriptionValidatable) ||
+      !validateInputs(peopleValidatable)
+    ) {
+      alert("Invalid inputs!");
+      return;
+    } else {
+      return [title, description, +people];
+    }
+  }
+
+  private clearInputs() {
+    const inputNames = ["title", "description", "people"];
+    for (const name of inputNames) {
+      (this as any)[`${name}InputElement`].value = "";
+    }
+  }
+
   @BindThis
   private handleFormSubmit(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
   }
 
   private configureForm() {
